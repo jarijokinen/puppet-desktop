cf4c02f5ad02e31ad40bb5c1d805316a22dca46c
Jari Jokinen <info@jarijokinen.com>
2013-05-08 23:20:05 +0300

    Move debian module into the s_desktop service

--- services/s_desktop/manifests/init.pp
+++ services/s_desktop/manifests/init.pp
@@ -1,8 +1,17 @@
 class s_desktop {
-  include "debian"
   include "chkrootkit"
   include "logcheck"
   include "logwatch"
   include "ntp"
   include "rkhunter"
+
+  package { "bsd-mailx": 
+    ensure => installed
+  }
+
+  file { "/etc/rc.local":
+    ensure  => present,
+    content => template("s_desktop/rc.local.erb"),
+    mode    => "0700"
+  }
 }
